9417a098ab18be0310fe073bd376ed98ea09c859
Alisherov-044 <alisherov044@gmail.com>
2023-04-16 16:36:31 +0500

    fixed bag axios (full)

--- src/lib/Axios.jsx
+++ src/lib/Axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BaseURL = "127.0.0.1:8000/api";
 
-function Axios(method, url, data) {
+function Axios(method, url, data = {}) {
   return axios({
     method: method,
     url: `${BaseURL}/${url}`,
